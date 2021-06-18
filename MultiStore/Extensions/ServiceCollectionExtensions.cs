e90b23a9dd27fb454be6c9a7d3cdd3d99f908fdc
Sergio Joel Ferreras Batista <sergiojoel9877@hotmail.com>
2021-06-18 18:44:37 -0400

    Create ServiceCollectionExtensions

---
+++ MultiStore/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using MultiStore.Data.Entities;
+using MultiStore.Data.Repositories;
+using MultiStore.Interfaces.Repositories;
+
+namespace MultiStore.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+    }
+}
