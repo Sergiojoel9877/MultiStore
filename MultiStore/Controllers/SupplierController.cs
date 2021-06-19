02061f3032ef86eaae21fe4b9f4702c502c36ce6
Sergio Joel Ferreras Batista <sergiojoel9877@hotmail.com>
2021-06-19 02:53:24 -0400

    Create SupplierController.cs

---
+++ MultiStore/Controllers/SupplierController.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultiStore.Data.Entities;
+using MultiStore.Interfaces.Services;
+using MultiStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MultiStore.Controllers
+{
+    public class SupplierController : Controller
+    {
+        readonly ISupplierService _supplierService;
+
+        public SupplierController(ISupplierService supplierService)
+        {
+            _supplierService = supplierService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetById(int id)
+        {
+            var data = await _supplierService.Get(id);
+            return View(data);
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _supplierService.GetAll();
+            return View(data);
+        }
+
+        public async Task<IActionResult> PostArticle([FromBody] Supplier supplier)
+        {
+            if (supplier == null)
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            await _supplierService.Create(supplier);
+            return View();
+        }
+
+        public async Task<IActionResult> Put([FromBody] Supplier supplier)
+        {
+            if (supplier == null)
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            await _supplierService.Update(supplier);
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id < 0)
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            await _supplierService.Delete(id);
+            return View();
+        }
+    }
+}
