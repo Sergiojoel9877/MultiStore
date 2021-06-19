cf1b332a64130fb33f0095beabe4190198d8bbd4
Ronald Ogando <ronaldogando150@gmail.com>
2021-06-18 23:04:36 -0400

    add repositories to unit of work interface

--- MultiStore/Interfaces/Repositories/IUnitOfWork.cs
+++ MultiStore/Interfaces/Repositories/IUnitOfWork.cs
@@ -5,6 +5,15 @@ namespace MultiStore.Interfaces.Repositories
 {
     public interface IUnitOfWork :IDisposable
     {
+        public IArticleRepository ArticleRepository { get; }
+        public IArticleRequestRepository ArticleRequestRepository { get; }
+        public IBrandRepository BrandRepository { get; }
+        public IDepartmentRepository DepartmentRepository { get; }
+        public IEmployeeRepository EmployeeRepository { get; }
+        public IPurchaseOrderRepository PurchaseOrderRepository { get; }
+        public ISupplierRepository SupplierRepository { get; }
+        public IUnitMeasurementRepository UnitMeasurementRepository { get; }
+
         void SaveChanges();
         Task SaveChangesAsync();
     }
