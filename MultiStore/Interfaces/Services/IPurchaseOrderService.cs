86e8ff9d34a2a5a3fa5dccf4d87b832ed8cd7f59
Sergio Joel Ferreras Batista <sergiojoel9877@hotmail.com>
2021-06-18 22:01:31 -0400

    Create IPurchaseOrderService.cs

---
+++ MultiStore/Interfaces/Services/IPurchaseOrderService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using MultiStore.Data.Entities;
+
+namespace MultiStore.Interfaces.Services
+{
+    public interface IPurchaseOrderService : IBaseService<PurchaseOrder>
+    {
+    }
+}
