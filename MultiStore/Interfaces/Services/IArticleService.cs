7ba945a18738685d40ad003bd2684196bd2558cd
Sergio Joel Ferreras Batista <sergiojoel9877@hotmail.com>
2021-06-18 22:01:20 -0400

    Create IArticleService.cs

---
+++ MultiStore/Interfaces/Services/IArticleService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MultiStore.Data.Entities;
+
+namespace MultiStore.Interfaces.Services
+{
+    public interface IArticleService : IBaseService<Article>
+    {
+    }
+}
