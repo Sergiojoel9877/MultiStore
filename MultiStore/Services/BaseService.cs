222c4834dcc5498a4d3738673d7068b97a7f7949
Sergio Joel Ferreras Batista <sergiojoel9877@hotmail.com>
2021-06-19 03:02:29 -0400

    Create BaseService.cs

---
+++ MultiStore/Services/BaseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MultiStore.Data.Repositories;
+using MultiStore.Interfaces.Repositories;
+using MultiStore.Interfaces.Services;
+
+namespace MultiStore.Services
+{
+    public abstract class BaseService
+    {
+        public readonly IUnitOfWork UnitOfWork;
+
+        public BaseService(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+    }
+}
