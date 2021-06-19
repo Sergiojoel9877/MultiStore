aa975698c845546fdb099c85fe05464822ad3f8d
Sergio Joel Ferreras Batista <sergiojoel9877@hotmail.com>
2021-06-19 02:52:48 -0400

    Create ArticleController.cs

---
+++ MultiStore/Controllers/ArticleController.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultiStore.Data.Entities;
+using MultiStore.Interfaces.Services;
+using MultiStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MultiStore.Controllers
+{
+    public class ArticleController : Controller
+    {
+        readonly IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetById(int id)
+        {
+            var data = await _articleService.Get(id);
+            return View(data);
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _articleService.GetAll();
+            return View(data);
+        }
+
+        public async Task<IActionResult> PostArticle([FromBody] Article article)
+        {
+            if(article == null)
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            await _articleService.Create(article);
+            return View();
+        }
+
+        public async Task<IActionResult> Put([FromBody] Article article)
+        {
+            if (article == null)
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            await _articleService.Update(article);
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id < 0)
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            await _articleService.Delete(id);
+            return View();
+        }
+    }
+}
