7d9961ad05c2ef5c0da94a534545152f81a5bfb8
Sergio Joel Ferreras Batista <sergiojoel9877@hotmail.com>
2021-06-19 02:53:16 -0400

    Create EmployeeController.cs

---
+++ MultiStore/Controllers/EmployeeController.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultiStore.Data.Entities;
+using MultiStore.Interfaces.Services;
+using MultiStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MultiStore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetById(int id)
+        {
+            var data = await _employeeService.Get(id);
+            return View(data);
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _employeeService.GetAll();
+            return View(data);
+        }
+
+        public async Task<IActionResult> PostArticle([FromBody] Employee employee)
+        {
+            if (employee == null)
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            await _employeeService.Create(employee);
+            return View();
+        }
+
+        public async Task<IActionResult> Put([FromBody] Employee employee)
+        {
+            if (employee == null)
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            await _employeeService.Update(employee);
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id < 0)
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            await _employeeService.Delete(id);
+            return View();
+        }
+    }
+}
