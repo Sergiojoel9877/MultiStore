fa570c7a474973e678b1404d9c427a8936cbb219
Sergio Joel Ferreras Batista <sergiojoel9877@hotmail.com>
2021-06-19 17:34:58 -0400

    Update ServiceCollectionExtensions.cs

--- MultiStore/Extensions/ServiceCollectionExtensions.cs
+++ MultiStore/Extensions/ServiceCollectionExtensions.cs
@@ -24,6 +24,7 @@ namespace MultiStore.Extensions
             services.AddScoped<IEmployeeService, EmployeeService>();
             services.AddScoped<IPurchaseOrderService, PurchaseOrderService>();
             services.AddScoped<ISupplierService, SupplierService>();
+            services.AddScoped<IUnitMeasurementService, UnitMeasurementService>();
         }
     }
 }
