b00d1370f07f07658aa74011ba8d9d0fb9b5c84d
Sergio Joel Ferreras Batista <sergiojoel9877@hotmail.com>
2021-06-18 22:02:51 -0400

    Create BrandService.cs

---
+++ MultiStore/Services/BrandService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MultiStore.Data.Entities;
+using MultiStore.Data.Repositories;
+using MultiStore.Interfaces.Repositories;
+using MultiStore.Interfaces.Services;
+
+namespace MultiStore.Services
+{
+    public class BrandService : IBrandService
+    {
+        bool disposedValue;
+
+        readonly UnitOfWork UnitOfWork;
+
+        public BrandService(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = (UnitOfWork)unitOfWork;
+        }
+
+        public async Task Create(Brand entity)
+        {
+            await UnitOfWork.BrandRepository.Add(entity);
+            await UnitOfWork.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            await UnitOfWork.BrandRepository.Delete(id);
+            await UnitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<Brand> Get(int id)
+        {
+            var data = await UnitOfWork.BrandRepository.GetById(id);
+            return data ?? null;
+        }
+
+        public async Task<IEnumerable<Brand>> GetAll()
+        {
+            var tcs = new TaskCompletionSource<IEnumerable<Brand>>();
+            try
+            {
+                var data = UnitOfWork.BrandRepository.GetAll();
+                tcs.SetResult(data);
+                return await tcs.Task;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ex {ex}");
+                tcs.SetResult(null);
+                tcs.SetException(ex);
+                return await tcs.Task;
+            }
+        }
+
+        public async Task Update(Brand article)
+        {
+            await Task.Yield();
+            UnitOfWork.BrandRepository.Update(article);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    UnitOfWork?.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
+
